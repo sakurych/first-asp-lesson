593e00a38ba526b56cb7a9ab6990f8802ae3aa1a
Sakurych <kolonka.power@mail.ru>
2023-01-30 17:23:44 +0400

    first commit

---
+++ WebApplication1/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication1/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+
+        public int Age { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [StringLength(30, ErrorMessage = "Не менее 30 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+
+        public string Message { get; set; }
+    }
+}
